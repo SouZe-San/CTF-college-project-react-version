41f319c9d76eb331ba2a550ca74a81c4fd36033f
SouZe-San <2003soumyajit@gmial.com>
2023-08-01 23:09:47 +0530

    some shifting

--- src/pages/Challenges/page.jsx
+++ src/pages/Challenges/page.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 // import Challenges from "./Challenges";
-// import { challenges_array } from "../../assets/Json/Allchallenges";
-import Challenges from "../../components/Challenges/Challenges";
+import { challenges_array } from "../../assets/Json/Allchallenges";
+import Challenges from "../../components/Challenges/ChallengeBlock/Challenges";
 import NotValid from "../../components/notValid/NotValid";
 
 const page = () => {
-  const challenges_array = null;
+  // const challenges_array = null;
   return (
     <div className="md:mx-12  mt-24 pt-16">
       {challenges_array ? <Challenges challenges_array={challenges_array} /> : <NotValid />}
