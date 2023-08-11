11f4d27a34f40e253d9443f3fcbf466bb3450683
Biswajit <biswajitmajumderjee@gmail.com>
2023-08-12 01:45:15 +0530

    constang-changes

--- src/components/navbar/smallNav.jsx
+++ src/components/navbar/smallNav.jsx
@@ -1,13 +1,9 @@
 import { Link } from "react-router-dom";
-// import homeSvg from "../../assets/nav-section-svgs/home-line.svg";
-// import profileSvg from "../../assets/nav-section-svgs/user_line.svg";
 import "./smallNavStyles.scss";
 function SmallNav() {
   return (
     <nav className="smallNavigation">
-      <div className="btns ">
-        <div className="menu">
-          <ul className="flex justify-between items-center">
+          <ul >
             <li>
               <Link to="/">
               <svg viewBox="0 0 24 24" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg"><path d="M12 3s-6.186 5.34-9.643 8.232A1.041 1.041 0 0 0 2 12a1 1 0 0 0 1 1h2v7a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 .98.98 0 0 0-.383-.768C18.184 8.34 12 3 12 3z"/></svg>
@@ -28,8 +24,6 @@ function SmallNav() {
               </Link>
             </li>
           </ul>
-        </div>
-      </div>
     </nav>
   );
 }
